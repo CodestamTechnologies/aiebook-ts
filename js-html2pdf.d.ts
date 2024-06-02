4a2f6412a7578f5acc09beed0b1bd7ca6318b099
souravvmishra <souravvmishra@gmail.com>
2024-06-02 13:13:00 +0530

    fixed ui

--- js-html2pdf.d.ts
+++ js-html2pdf.d.ts
@@ -1,3 +1,3 @@
 // This file is used to remove the type-errors in js-html2pdfmodule
 
-declare module 'html2pdf.js';
+declare module 'js-html2pdf';
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import { gemini_completion } from "@/gemini";
 import MarkdownEditor from "@/components/MarkdownEditor";
 import { useReactToPrint } from 'react-to-print';
-import Html2Pdf from 'html2pdf.js';
+import Html2Pdf from 'js-html2pdf';
 
 interface SubChapter {
     name: string;
@@ -33,7 +33,8 @@ export default function Homepage() {
                 if (document) {
                     setLoading(true);
                     const html = document.getElementsByTagName("html")[0];
-                    await Html2Pdf().from(html).save();
+                    const exporter = new Html2Pdf(html);
+                    await exporter.getPdf(true);
                     await setLoading(false);
                 }
             }
